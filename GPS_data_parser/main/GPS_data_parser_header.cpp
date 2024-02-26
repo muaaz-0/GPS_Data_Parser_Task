88f5bef12258fcc8ac73d12a8fff73a14b45153d
muaaz-0 <muaazulhassan786@gmail.com>
2024-02-26 23:50:27 +0500

    making a separate start parsing sentence
    here i have made separate function where string checksum is found and packet integrity if verified

--- GPS_data_parser/main/GPS_data_parser_header.cpp
+++ GPS_data_parser/main/GPS_data_parser_header.cpp
@@ -4,68 +4,78 @@
 using namespace std;
 #include "GPS_data_parser_header.h"
 
-int parse_gps_data(string Sentence) {
+
+char calculated_checksum_hex[3];
+GPSData GPSdata;                             //Creating the variable of Structure
+int Start_Parsing(string packet)
+
+{
     // Calculate checksum
+    int i=0;
     unsigned int checksum = 0;
-    for (int i = 1; packet[i] != '*' && packet[i] != '\0'; i++) {
-        checksum ^= packet[i];
+    for (i = 1; packet[i] != '*' && packet[i] != '\0'; i++) // This loop runns until a steric is found or a NULL char
+    {
+        checksum ^= packet[i];                             // XOR operator is applied on every element in the packet to find checksum
+    }
+    
+char provided_checksum[2];
+      if (packet[i]=='*') //extract provided checksum
+        {
+          provided_checksum[0]=packet[i+1];  //store first element next to steic in provided_checksum[0]
+          provided_checksum[1]=packet[i+2];  //store second ....               ..   provided_checksum[0]
+          provided_checksum[2]='\0';         //store last element as null
+        }
+         // check if steric is present or not if not then no checksum found
+    
+
+    if (packet[i] != '*')                   //If steric is not found this means sentence is not correct
+    {
+        cout<<"Checksum Not Found! '-': "<<endl;
+        return 0;
     }
 
-    // Verify checksum presence
-    int i = 0;
-    while (packet[i] != '*' && packet[i] != '\0') {
-        i++;
-    }
-    if (packet[i] != '*') {
-        printf("No checksum found! '-' :");
-        return -1;
-    }
-
-    // Extract provided checksum
-    char provided_checksum[3];
-    strncpy(provided_checksum, packet + i + 1, 2);
-    provided_checksum[2] = '\0';
 
     // Compute checksum in hexadecimal
-    char calculated_checksum_hex[3];
-    snprintf(calculated_checksum_hex, sizeof(calculated_checksum_hex), "%02X", checksum);
-
-int Noof_empty_data=0;
-int empty_count=0;
-int empty_data_pos[15];
-
-
-    // Check sentence format
-    int commas = 0;
-    for (int i = 0; i < strlen(packet); i++) {
-        if (packet[i] == ',') {
-            commas++;
-        }
-        //checking for empty data positions
-            if (packet[i] == ',' && packet[i+1] == ',') {
-                empty_data_pos[empty_count]=commas;
-                
-                printf("empty data position is%d\n", empty_data_pos[empty_count]);
-                empty_count++;
-                Noof_empty_data=empty_count;
-                printf("empty parameter position is: %d\n", Noof_empty_data);
-
-            }
-        }
     
-    if (strncmp(packet, "$GPGGA,", 7) == 0 && commas == 14 && packet[i] == '*') {
-        printf("The sentence is in GGA format: "); // GGA sentence
+    snprintf(calculated_checksum_hex, sizeof(calculated_checksum_hex), "%02X", checksum); // converters the decimal value to hexadecimal value
+
+
+
+    int commas = 0;
+    for (int i = 0; i < strlen(packet.c_str()); i++) // extract the number of commas in the packet
+    {
+        if (packet[i] == ',')                        // if comma found then increment the commas variable
+        {
+            commas++;
+        }       
+    }
+    // This section compares the first 7 character of the packet with GPGGA to confirm sentence as GGA and check if threre are 14 commas
+
+    if (strncmp(packet.c_str(), "$GPGGA,", 7) == 0 && commas == 14 && packet[i] == '*')//c_str() function converts the string packet to a const character
+     {
+        cout<<"The sentence is in GGA format: \n"<<endl;
+
     } else {
-        printf("The sentence is not in GGA format: ");
+       cout<<"The sentence is not in GGA format: \n"<<endl;
+        return 0;
     }
 
     // Check packet integrity
+    
     if (strcmp(provided_checksum, calculated_checksum_hex) == 0) {
+        cout<<"Packet integrity valid: chksum \n"<< calculated_checksum_hex<<endl;
 
-        printf("Packet integrity valid chksum \n", calculated_checksum_hex);
+        manage_missing(packet);  //if packet integrity valid then manage manage missing function is called
     } else {
-        printf("Packet integrity not valid \n", calculated_checksum_hex );
+        cout<<"Packet integrity not valid: chksum \n"<< calculated_checksum_hex<<endl;   // if packet intefrity is not valid then return 0
+        //return 0;
+        //manage_missing(packet); 
     }
+    return 0;
+}
+
+int parse_gps_data(string Sentence) {
+    
 
     // Extract packet data excluding '$' and '*'
     char packet2[strlen(packet) - 3];
